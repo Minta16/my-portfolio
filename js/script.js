4b9cc50fbc2cb4a6ce8a5016241286208f99dc70
Minta16 <thiennhan09420@gmail.com>
2026-03-29 20:43:50 +0700

    up lần 1

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Lê Hoàng Thiện Nhân đã load xong!');
+
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+});
